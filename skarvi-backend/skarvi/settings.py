3196d72243673ec77f83caf3d280ee6379a9d75a
Gopinath <gopisachin95@gmail.com>
2025-05-23 02:16:28 +0530

    changes and cloud db integration

--- skarvi-backend/skarvi/settings.py
+++ skarvi-backend/skarvi/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from dotenv import load_dotenv
+load_dotenv()
+
 from pathlib import Path
 import datetime
 import dj_database_url
 from datetime import timedelta
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
