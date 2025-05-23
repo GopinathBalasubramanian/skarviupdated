c7fba2054abed04a4c2c1e3e68b0a6ededfbd74b
Gopinath <gopisachin95@gmail.com>
2025-05-23 22:10:43 +0530

    settings.py changes

--- skarvi-frontend/src/components/pages/login.tsx
+++ skarvi-frontend/src/components/pages/login.tsx
@@ -3,9 +3,10 @@ import axios from 'axios';
 import { API_URL } from '../../utils/utils';
 
 export function loginUser(username: string, password: string) {
-  return axios.post(`${API_URL}/api/token/`, { username, password }, {
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
+  axios.post(`${API_URL}/api/token`, { username, password }, {
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
 }
