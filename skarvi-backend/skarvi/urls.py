74a277fffe466c1652ff8e394073907ebcd9cf2e
Gopinath <gopisachin95@gmail.com>
2025-05-23 20:24:42 +0530

    Add JWT login endpoint /api/token/

--- skarvi-backend/skarvi/urls.py
+++ skarvi-backend/skarvi/urls.py
@@ -8,6 +8,8 @@ from django.conf import settings
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,8 +24,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('login.urls')), 
+   #  path('paper_trades/', include('paper_trades.urls')),
+   #  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+   
+   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('paper_trades/', include('paper_trades.urls')),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 
 
     # Swagger and Redoc URLs
