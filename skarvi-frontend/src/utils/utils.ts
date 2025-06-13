4bfd0bbf44d93dba9d74d953560bf5240f3e9201
gopinath <gopisachin95@gmail.com>
2025-06-14 02:44:37 +0530

    render changed in settings.py

--- skarvi-frontend/src/utils/utils.ts
+++ skarvi-frontend/src/utils/utils.ts
@@ -1,4 +1,5 @@
 // export const API_URL = 'http://127.0.0.1:8000';
 // utils.ts
 // export const API_URL = import.meta.env.VITE_API_URL.replace(/\/+$/, '');
-export const API_URL = "skarvisystems.onrender.com"
+// utils/utils.ts
+export const API_URL = "https://skarvisystems.onrender.com"; 
