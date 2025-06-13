10a53ee4c0c1a25e7f0a09f824947b65ab07640b
gopinath <gopisachin95@gmail.com>
2025-06-14 02:24:10 +0530

    render changed in settings.py

--- skarvi-backend/skarvi/settings.py
+++ skarvi-backend/skarvi/settings.py
@@ -151,7 +151,7 @@ CORS_ALLOWED_ORIGINS = [
     # "http://localhost:8000",
     "https://skarvi-frontend-cloud.vercel.app",
     "https://skarvi-frontend-cl-git-ef9172-gopinathbalasubramanians-projects.vercel.app",
-    "skarvisystems.onrender.com",
+    "https://skarvisystems-fkxwmohjz-gopinathbalasubramanians-projects.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
