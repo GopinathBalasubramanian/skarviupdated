b9ea585f895755061ac55dc686632fea0373be55
Gopinath B <122074967+GopinathBalasubramanian@users.noreply.github.com>
2025-06-16 22:57:47 +0530

    Update urls.py

--- skarvi-backend/login/urls.py
+++ skarvi-backend/login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import LoginAPIView, LogoutView 
 
 urlpatterns = [
-    path('token/', LoginAPIView.as_view(), name='token_obtain_pair'),
+    path('login/token/', LoginAPIView.as_view(), name='token_obtain_pair'),
     path('auth/logout/', LogoutView.as_view(), name='token_blacklist'), 
 ]
